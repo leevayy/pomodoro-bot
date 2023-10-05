324f85e7e91533e842af2f27524b8c004ea3a52c
leevayy <maoaoa7@gmail.com>
2023-10-05 15:09:16 +0300

    minor refactoring

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from bot.botConnection import bot
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-  bot.send_message(message.chat.id,"Привет ✌️ ")
-bot.infinity_polling()
+def start_polling():
+    @bot.message_handler(commands=['start'])
+    def start_message(message):
+        bot.send_message(message.chat.id,"Привет ✌️ ")
+    
+    bot.infinity_polling()
+
+if __name__ == "__main__":
+    start_polling()
+
